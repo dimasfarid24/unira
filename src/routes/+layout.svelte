488c81f38e5f1daab8ecd1d7f3ba799400c0340b
dimasfarid24 <dimasfarid10@gmail.com>
2024-07-21 23:55:55 +0700

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { afterNavigate } from "$app/navigation";
+    import "../app.css";
+  import Body from "./layouts/Body.svelte";
+  import Footer from "./layouts/Footer.svelte";
+  import Navbar from "./layouts/Navbar.svelte";
+
+    afterNavigate(() =>{
+        window.HSStaticMethods.autoInit();
+    })
+</script>
+
+<Navbar />
+
+<Body>
+    <slot></slot>
+</Body>
+
+<Footer />
---
+++ src/routes/layouts/Navbar.svelte
@@ -0,0 +1,60 @@
+<!-- src/routes/components/Navbar.svelte -->
+<script>
+    // Script untuk komponen Navbar
+</script>
+
+<style>
+    /* Gaya untuk komponen Navbar */
+</style>
+
+<!-- <nav>
+    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800">
+        <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+            <div class="flex items-center justify-between">
+                <a class="flex-none text-xl font-semibold dark:text-white" href="/">Brand</a>
+                <div class="sm:hidden">
+                    <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10" data-hs-collapse="#navbar-with-collapse" aria-controls="navbar-with-collapse" aria-label="Toggle navigation">
+                    <svg class="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+                    <svg class="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                    </button>
+                </div>
+            </div>
+            <div id="navbar-with-collapse" class="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block">
+                <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+                    <a class="font-medium text-blue-500" href="/" aria-current="page">Landing</a>
+                    <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" href="/">Account</a>
+                    <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" href="/">Work</a>
+                    <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" href="/">Blog</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+</nav> -->
+
+<nav>
+    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-blue-950 text-sm py-4">
+        <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+          <div class="flex items-center justify-between">
+            <a class="flex-none text-xl font-semibold grid grid-cols-2 bg-blue-200" href="/">
+                <img src="https://unira.ac.id/img/logo.png" class="bg-white col-start-1 w-16 h-16" alt="">
+                <strong class="bg-slate-100">UNIVERSITAS MADURA</strong>
+                <small>Jl. Pangleghur</small>
+            </a>
+            <div class="sm:hidden">
+              <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" data-hs-collapse="#navbar-with-collapse" aria-controls="navbar-with-collapse" aria-label="Toggle navigation">
+                <svg class="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+                <svg class="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+              </button>
+            </div>
+          </div>
+          <div id="navbar-with-collapse" class="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block">
+            <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+              <a class="font-medium text-blue-500" href="#" aria-current="page">Landing</a>
+              <a class="font-medium text-gray-600 hover:text-gray-400" href="#">Account</a>
+              <a class="font-medium text-gray-600 hover:text-gray-400" href="#">Work</a>
+              <a class="font-medium text-gray-600 hover:text-gray-400" href="#">Blog</a>
+            </div>
+          </div>
+        </nav>
+      </header>
+</nav>
